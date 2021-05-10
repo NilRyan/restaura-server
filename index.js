19d2cf4528ed78f45b373c39156de3efe74dcc57
Neil Ryan <neilryanlipaod@gmail.com>
2021-05-10 19:02:49 +0800

    CONFIGURED all routes

--- index.js
+++ index.js
@@ -4,7 +4,31 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT;
+app.get(
+  "/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: ["mcdonalds", "wendys"]
+      },
+    })
+  }
+)
+
+app.get("/api/v1/restaurants/:id", (req, res) => {
+   console.log(req.params);
+   res.status(200).json({
+     meow: "meow"
+   })
+  }
+)
+
+app.post("/api/v1/restaurants/", (req, res) => {
+  console.log(req)
+  
+})
+
+const port =  process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`server is up and listening on port ${port}`)
 })
